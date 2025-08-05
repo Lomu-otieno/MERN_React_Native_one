c8c4c489772f67ef0a447d4fb66356659a9219ed
lomuotieno <lomuotieno@gmail.com>
2025-08-05 17:43:16 +0300

    cleaning cloudinary folders

--- backend/source/lib/cloudinary.js
+++ backend/source/lib/cloudinary.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "dating_app_photos",
+    folder: "dating_app",
     allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
