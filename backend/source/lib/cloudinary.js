958f1ea67194b823c8a580efd5a8ff195ae12e9a
lomuotieno <lomuotieno@gmail.com>
2025-08-05 17:20:51 +0300

    freezing cloudinary folder

--- backend/source/lib/cloudinary.js
+++ backend/source/lib/cloudinary.js
@@ -2,17 +2,17 @@ import { v2 as cloudinary } from "cloudinary";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: "dating_app_photos",
-        allowed_formats: ["jpg", "png", "jpeg"],
-    },
+  cloudinary,
+  params: {
+    // folder: "dating_app_photos",
+    allowed_formats: ["jpg", "png", "jpeg"],
+  },
 });
 
 export { cloudinary, storage };
