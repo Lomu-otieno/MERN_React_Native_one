ec7b451e30cee43721b0fea41b01f7bafea3d4e1
lomuotieno <lomuotieno@gmail.com>
2025-08-08 19:52:06 +0300

    cors

--- backend/source/index.js
+++ backend/source/index.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:5500",
     credentials: true, // Allow cookies/auth headers
   })
 );
