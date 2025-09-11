0b667f640d2b2949473eaeb331e66f56db33b799
lomuotieno <lomuotieno@gmail.com>
2025-09-11 22:13:42 +0300

    deploy prepare

---
+++ lomu/app.config.js
@@ -0,0 +1,57 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "lomu",
+    slug: "lomu",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/splash_lomu.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash_icon.png",
+      resizeMode: "cover",
+      backgroundColor: "#000000",
+    },
+    ios: {
+      splash: {
+        image: "./assets/splash_lomu.png",
+        resizeMode: "cover",
+        backgroundColor: "#000000",
+      },
+      supportsTablet: true,
+      bundleIdentifier: "com.lomu.lomu",
+      buildNumber: "1.0.0",
+    },
+    android: {
+      splash: {
+        image: "./assets/splash_icon.png",
+        resizeMode: "cover",
+        backgroundColor: "#000000",
+      },
+      adaptiveIcon: {
+        foregroundImage: "./assets/splash_lomu.png",
+        backgroundColor: "#000000",
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.lomu.lomu",
+      versionCode: 1,
+      permissions: [],
+    },
+    web: {
+      favicon: "./assets/splash_lomu.png",
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    extra: {
+      eas: {
+        projectId: "46f20cfb-2119-4700-a273-3dd63d7b73dc",
+      },
+      backendUri: process.env.BACKEND_URI,
+      adminId: process.env.ADMIN_ID,
+    },
+  },
+};
