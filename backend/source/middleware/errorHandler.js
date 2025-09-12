7813de2000da5d5511c3083ca8b02f0bd7a29d20
lomuotieno <lomuotieno@gmail.com>
2025-09-13 01:28:59 +0300

    expo-doctor

--- backend/source/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode || 500;
-    res.status(statusCode).json({
-        message: err.message || "Internal Server Error",
-    });
-};
-
-export default errorHandler;
