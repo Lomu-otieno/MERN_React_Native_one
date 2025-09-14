1b59f3e0d2b7834e4e67afbb3b3c07a387ba7323
lomuotieno <lomuotieno@gmail.com>
2025-09-15 00:57:29 +0300

    fix: update gitignore and remove native folders

---
+++ lomu/Components/RNViewOverflow.js
@@ -0,0 +1,9 @@
+// components/RNViewOverflow.js
+import React from "react";
+import { View } from "react-native";
+
+const RNViewOverflow = (props) => {
+  return <View {...props} />;
+};
+
+export default RNViewOverflow;
