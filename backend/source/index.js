c761d41b958dfdde9f02f0b6cc57559c48a3f7d7
Lomu-otieno <116675442+Lomu-otieno@users.noreply.github.com>
2025-09-19 23:32:06 +0300

    Update index.js
    json

--- backend/source/index.js
+++ backend/source/index.js
@@ -47,7 +47,7 @@ app.use(
 
 // Routes
 app.get("/healthz", (req, res) => res.send("API is running..."));
-app.get("/", (req, res) => res.send("Server is running."));
+app.get("/", (req, res) => res.json({mesage: "Server is running!"});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
